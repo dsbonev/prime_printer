f0cfb0aeb887cbc0922f4c8c44ba8e72bf8141d9
Dimitar Bonev <dsbonev@gmail.com>
2015-01-06 12:05:14 +0200

    publish gem

--- spec/prime_printer_spec.rb
+++ spec/prime_printer_spec.rb
@@ -1,6 +1,5 @@
 
 describe PrimePrinter do
-  #  upload to rubygems, reuse initialize keyword-to-instance mapping, test on other virtual machine
   it 'should print out a multiplication table of the first 10 prime numbers' do
     table = <<-TABLE
     |   2   3   5   7  11  13  17  19  23  29
